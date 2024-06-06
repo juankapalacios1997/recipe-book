096315139e7b0a14daa6240efa4e3cfa54e5054a
juankapalacios1997 <jcmaster_1997@hotmail.com>
2024-06-06 08:36:23 -0600

    First changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesListComponent } from './recipe-book/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './recipe-book/recipes-details/recipes-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { RecipeItemComponent } from './recipe-book/recipes-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeBookComponent,
+    HeaderComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    ShoppingListComponent,
+    NavbarComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
